a55876d29bcc4d1b95b8da118fabeb05239e82cf
RAKOTONARIVO ANDRIANJAKA-BRICE <andrianjaka-brice.rakotonarivo@etu.unistra.fr>
2022-03-27 14:47:47 +0000

    Update vue.config.js

--- vue.config.js
+++ vue.config.js
@@ -1,7 +1,7 @@
 const { defineConfig } = require('@vue/cli-service')
 module.exports = defineConfig({
   transpileDependencies: true,
-  publicPat: process.env.NODE_ENV === 'production' ? '/WebFront/' : '/',
+  publicPath: process.env.NODE_ENV === 'production' ? '/WebFront/' : '/',
   pluginOptions: {
     i18n: {
       locale: 'en',
