b12cca88a76b1de8bb1b553371bea27c8f41c471
Elias Mouaheb <elias.mouaheb@etu.unistra.fr>
2022-05-10 12:05:40 +0200

    mise à jour env

--- src/services/ws.js
+++ src/services/ws.js
@@ -1,5 +1,5 @@
 import createStore from '../store'
 
-const socket = new WebSocket(process.env.VUE_APP_WEBSOCKET__URL_LOBBY+'/ws/lobby?token=' +createStore.getters.id);
+const socket = new WebSocket(process.env.VUE_APP_WEBSOCKET_URL_LOBBY+'/ws/lobby?token=' +createStore.getters.id);
 
 export default socket;
