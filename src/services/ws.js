1de1d48e92bd1f54c308929f7f6eb6bfb72f6095
Berehovenko <rakotonarivobrice@gmail.com>
2022-05-10 16:00:42 +0200

    fix webGL

--- src/services/ws.js
+++ src/services/ws.js
@@ -1,4 +1,5 @@
 const userId = JSON.parse(localStorage.getItem('own-id'))
 const socket = new WebSocket(process.env.VUE_APP_WEBSOCKET_URL_LOBBY+'/ws/lobby?token=' + userId);
+//ws://monopoly.schwanndev.fr/ws/lobby?token=8119e870-60d5-4934-aa29-814c169eb099
 
 export default socket;
