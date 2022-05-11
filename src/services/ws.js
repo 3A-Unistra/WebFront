50f5e58e3d05d05f7391510357eff3743cb9f2fe
Berehovenko <rakotonarivobrice@gmail.com>
2022-05-11 19:21:52 +0200

    changement de l'uuid par le token dans ws.js

--- src/services/ws.js
+++ src/services/ws.js
@@ -1,5 +1,4 @@
-const userId = JSON.parse(localStorage.getItem('own-id'))
-const socket = new WebSocket(process.env.VUE_APP_WEBSOCKET_URL_LOBBY+'/ws/lobby?token=' + userId);
-//ws://monopoly.schwanndev.fr/ws/lobby?token=8119e870-60d5-4934-aa29-814c169eb099
+const userToken = JSON.parse(localStorage.getItem('user-token'));
+const socket = new WebSocket(process.env.VUE_APP_WEBSOCKET_URL_LOBBY+'/ws/lobby?token=' + userToken);
 
 export default socket;
