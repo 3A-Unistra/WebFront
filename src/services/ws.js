f97b299f27dd5e22babdcbd96b448003c26d2dcb
Berehovenko <rakotonarivobrice@gmail.com>
2022-05-18 02:05:41 +0200

    pleins de fix

--- src/services/ws.js
+++ src/services/ws.js
@@ -1,4 +1,4 @@
-const userToken = JSON.parse(localStorage.getItem('own-id'));
+const userToken = JSON.parse(localStorage.getItem('user-token'));
 const socket = new WebSocket(process.env.VUE_APP_WEBSOCKET_URL_LOBBY+'/ws/lobby?token=' + userToken);
 
 export default socket;
